859f6ad88407dc79411c4b86884d25e9f56f1c74
Fábio Gomes <fabio.gomes@monde.com.br>
2013-12-27 14:50:22 -0200

    De mostrar a lista de arquivos e não a lista esperada quando falhar o teste

--- drive_upload_test.go
+++ drive_upload_test.go
@@ -46,7 +46,7 @@ func TestObterListaArquivos_2Arquivos_NomeECaminhoRetornado(t *testing.T) {
 	for _, item := range lista {
 		caminho, ok := arquivos[item.Arquivo]
 		if !ok {
-			t.Errorf("Arquivo não retornado na lista: %v lista: %v", item.Arquivo, lista)
+			t.Errorf("Arquivo não retornado: %v arquivos: %v", item.Arquivo, arquivos)
 		} else if caminho != item.Caminho {
 			t.Errorf("Caminho inválido. Esperado %v retornado %v", item.Caminho, caminho)
 		}
